f724f2d332adf2155dd6a0247578eb0003412270
Damien Clement <damien.clement.czech@gmail.com>
2021-02-13 11:50:12 +0100

    Add Comment ad fix vector deleting.

--- src/framework/LayerStack.cpp
+++ src/framework/LayerStack.cpp
@@ -1,15 +1,21 @@
 #include "LayerStack.h"
-#include <algorithm>
 
 namespace Amber
 {
     LayerStack::~LayerStack()
     {
-        for(const auto& layer : this->m_layers)
+        //NOTE Removing raw pointer from vector is not really smart (-_-)
+        //NOTE So here you can look at this https://www.fluentcpp.com/2018/09/18/how-to-remove-pointers-from-a-vector-in-cpp/
+
+        //FIXME Getting error [deleting a pointer through a type that is not marked 'gsl::owner<>'; consider using a smart pointer instead]
+        //TODO Sollution is change raw pointer to smart pointer
+        for(auto& layer : this->m_layers)
         {
             layer->OnDetach();
             delete layer;
+            layer = nullptr;
         }
+        this->m_layers.erase(std::remove(this->m_layers.begin(), this->m_layers.end(), nullptr), this->m_layers.end());
     }
 
     void LayerStack::PushLayer(Layer* t_layer)
