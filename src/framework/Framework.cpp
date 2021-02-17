39c1f61bbe0acfca2bac159375dd2511fb3d76e2
Damien Clement <damien.clement.czech@gmail.com>
2021-02-17 16:49:09 +0100

    Remove comment.

--- src/framework/Framework.cpp
+++ src/framework/Framework.cpp
@@ -53,7 +53,6 @@ int main()
 {
     Amber::Logger::Init();
 
-    //FIXME Getting error [deleting a pointer through a type that is not marked 'gsl::owner<>'; consider using a smart pointer instead]
     //TODO Create smart pointer here
     auto framework = Amber::CreateFramework();
     framework->Run();
